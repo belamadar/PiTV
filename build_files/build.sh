7661ab29e5db0a68b3534e9700c03c885efc3c40
Csongor Bikki <68422834+belamadar@users.noreply.github.com>
2025-07-01 23:39:21 +0000

    fix: wrong repos

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,11 +2,7 @@
 set -euxo pipefail
 
 # Add RPM Fusion repos for Fedora (Free + Non-Free)
-curl -o /etc/yum.repos.d/rpmfusion-free.repo \
-  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free.repo
-
-curl -o /etc/yum.repos.d/rpmfusion-nonfree.repo \
-  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree.repo
+sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Add RPM packages
 dnf5 install \
