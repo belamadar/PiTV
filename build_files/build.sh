6cf092db589e57256c7bbc23e9f7c926fe646c09
Csongor Bikki <68422834+belamadar@users.noreply.github.com>
2025-07-01 23:34:26 +0000

    bug: missing repos

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+# Add RPM Fusion repos for Fedora (Free + Non-Free)
+curl -o /etc/yum.repos.d/rpmfusion-free.repo \
+  https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free.repo
+
+curl -o /etc/yum.repos.d/rpmfusion-nonfree.repo \
+  https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree.repo
+
 # Add RPM packages
 dnf5 install \
     weston \
