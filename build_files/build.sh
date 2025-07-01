f21c770a902afe49f652fa929b3006476403da8d
Csongor Bikki <68422834+belamadar@users.noreply.github.com>
2025-07-01 23:54:53 +0000

    fix: missing -y flag in install

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-# Add RPM Fusion repos for Fedora (Free + Non-Free)
-sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
 # Add RPM packages
-dnf5 install \
+dnf5 -y install \
     weston \
     flatpak \
     greetd \
